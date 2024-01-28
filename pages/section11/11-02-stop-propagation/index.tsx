97b9ced2f1ad9759ea760631183c3a04f53913c4
fieldnight <da.young.yun13@gmail.com>
2024-01-28 16:38:22 +0900

    fe

--- pages/section11/11-02-stop-propagation/index.tsx
+++ pages/section11/11-02-stop-propagation/index.tsx
@@ -4,17 +4,12 @@ import { Fragment } from "react";
 import { writer } from "repl";
 //fragment는 페이지의 리턴값에서 요소들을 감싸는 바깥<></>요소에 태그를 붙일때 주로 사용한다. 기존의 <div></div>는 아무래도 요소를 하나 더 그려야 하기 때문에 렌더링 속도가 저하된다. 특별한 이유를 (예: 내부 요소가 span으로 이루어져있을 경우) 제외하고는 지양해라.
 
-
 import { MouseEvent } from "react";
-
-
-
-
 const FETCH_BOARDS = gql`
   query {
     fetchBoards {
       number
-      writer 
+      writer
       title
       contents
     }
@@ -47,29 +42,42 @@ export default function StaticRoutedPage() {
   }; //event.target 이 바로 태그. target.id는 html에서 가져왔기에 문자열. 숫자로 변환
   //variables가 $. $number에 뒤에걸 보내줘.
 
-  const onClickAlert = (event : MouseEvent<HTMLDivElement>) => {
+  const onClickAlert = (event: MouseEvent<HTMLDivElement>) => {
     alert(`${event.currentTarget.id}님이 작성한 글입니다.`);
   };
 
+  const qqq1 = (event) => {
+    alert("1번 클릭");
+  };
 
-  //TS에서 제네릭을 지원하지 않는 MouseEvent를 사용하여 발생하는 문제: 이벤트 핸들러에서 제대로된 제네릭 타입을 사용하려면 React.MouseEvent를 사용해야 한다. 아니면~ MouseEvent를 react에서 import하면 된다. 
+  const qqq2 = (event) => {
+    event.stopPropagation();
+    alert("2번 클릭");
+  };
+
+  const qqq3 = (event) => {
+    event.stopPropagation();
+    alert("3번 클릭");
+  };
+
+  //TS에서 제네릭을 지원하지 않는 MouseEvent를 사용하여 발생하는 문제: 이벤트 핸들러에서 제대로된 제네릭 타입을 사용하려면 React.MouseEvent를 사용해야 한다. 아니면 MouseEvent를 react에서 import하면 된다.
   return (
     <div>
       {data?.fetchBoards.map((el: any, index) => (
-        <div key={el.number} id = {el.writer} onClick={onClickAlert}>
+        <div key={el.number} id={el.writer} onClick={qqq1}>
           {/* 맵으로 화면을 그릴 경우 반드시 key를 사용해 세트로 묶어라. 
 
         리엑트는 맵을 이용 할 경우 자체적으로 효율을 찾는 로직이 실행된다. 리엑트의 논리대로면 체크박스가 해당 열 span들과의 연관도가 낮아진다. 그래서 둘 사이에 고유한 값(겹치지 않는 값)을 key=value로 지정해서 react에게 이 둘이 짝임을 알려준다.
         
         이 구간은 React JSX 주석 형태이다. */}
-          <span>
-            <input type="checkbox" />
+          <span onClick={qqq2}>
+            <input type="checkbox" onClick={qqq3} />
           </span>
           <span style={{ margin: "10px" }}>{el.number}번 게시물 </span>
-          <span style={myStyles}>
-            제목: {el.title}
+          <span style={myStyles}>제목: {el.title}</span>
+          <span style={myStyles} id="writer">
+            작성자: {el.writer}
           </span>
-          <span style={myStyles} id="writer">작성자: {el.writer}</span>
           <span>
             <button id={el.number} onClick={onClickDelete}>
               삭제
